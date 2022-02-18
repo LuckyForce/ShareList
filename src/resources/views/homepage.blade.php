d310192d8ce7aa1421525396508976fc14d74fab
LuckyForce <adrian.schauer@aon.at>
2022-02-18 14:36:02 +0100

    Added header Footer

--- src/resources/views/homepage.blade.php
+++ src/resources/views/homepage.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>T3</title>
-</head>
-<body>
-    Hier wird ein heftiges Projekt entstehen.
-</body>
-</html>
+@include('header')
+Hier wird ein heftiges Projekt entstehen.
+@include('footer')
