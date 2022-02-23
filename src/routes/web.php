82dbf7310cd0649a8eb527400a165a382b458d56
LuckyForce <adrian.schauer@aon.at>
2022-02-23 11:24:04 +0100

    Added first frontend routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,10 +13,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//TODO: Delete
 Route::get('/', function () {
     return view('welcome');
 });
 
+//TODO: Change to /
 Route::get('/homepage', function () {
     return view('homepage');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/lists', function () {
+    return view('lists');
+});
+
+Route::get('/list/{id}', function ($id) {
+    return view('lists', ['id' => $id]);
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/user', function ($id) {
+    return view('user', ['id' => $id]);
 });
