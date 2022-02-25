c5aa5a1018dff6486b46b360a2595d96ec701775
LuckyForce <adrian.schauer@aon.at>
2022-02-25 13:05:08 +0100

    Added Query Test. Database Config Required!!!

--- src/resources/views/homepage.blade.php
+++ src/resources/views/homepage.blade.php
@@ -1,3 +1,9 @@
 @include('header')
 Hier wird ein heftiges Projekt entstehen.
+<?php
+$results = DB::select('select * from l_u_users', array(1));#
+foreach ($results as $result) {
+    echo $result->l_u_email;
+}
+?>
 @include('footer')
