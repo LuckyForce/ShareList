99fc90d3834731e2ef1ce68955e8a1cc89da5ca2
LuckyForce <adrian.schauer@aon.at>
2022-02-26 18:29:16 +0100

    Database Changes

--- documents/databasedb.sql
+++ documents/databasedb.sql
@@ -2,23 +2,23 @@
 -- auto generated ddl-script ###########################
 -- generated sql creation script for ER model
 -- database-#############################################
-drop database if exists databasedb;
-create database databasedb;
-use databasedb;
+-- drop database if exists databasedb;
+-- create database databasedb;
+-- use databasedb;
 -- switch autocommit off
-set autocommit=0;
+-- set autocommit=0;
 -- tables-#################################################
 -- table h_hasaccess
-create table h_hasaccess(
+create table sl_h_hasaccess(
      h_l_id varchar(100) not null,
      h_u_id varchar(100) not null,
-     h_p_id varchar(100) not null unique,
+     h_p_id varchar(100) not null,
      h_priviledge varchar(100) not null,
      primary key(h_l_id,h_u_id,h_p_id)
 );
 
 -- table l_list
-create table l_list(
+create table sl_l_list(
      l_id varchar(100) not null,
      l_createdat varchar(100) not null,
      l_title varchar(100) not null,
@@ -26,7 +26,7 @@ create table l_list(
 );
 
 -- table l_listitem
-create table l_listitem(
+create table sl_l_listitem(
      l_id varchar(100) not null,
      l_l_id varchar(100) not null,
      l_lastupdated varchar(100) not null,
@@ -35,14 +35,14 @@ create table l_listitem(
 );
 
 -- table p_priviledge
-create table p_priviledge(
+create table sl_p_priviledge(
      p_id varchar(100) not null,
      p_description varchar(100) not null,
      primary key(p_id)
 );
 
 -- table t_token
-create table t_token(
+create table sl_t_token(
      t_id varchar(100) not null,
      t_code varchar(100) not null,
      t_expirationdate varchar(100) not null,
@@ -51,7 +51,7 @@ create table t_token(
 );
 
 -- table u_user
-create table u_user(
+create table sl_u_user(
      u_id varchar(100) not null,
      u_name varchar(100) not null,
      u_email varchar(100) not null,
@@ -65,13 +65,13 @@ create table u_user(
 
 
 -- foreign keys-#################################################
-alter table h_hasaccess
-add foreign key (h_p_id) references p_priviledge(p_id) on delete restrict on update restrict,
-add foreign key (h_l_id) references l_list(l_id) on delete restrict on update restrict,
-add foreign key (h_u_id) references u_user(u_id) on delete restrict on update restrict;
-alter table l_listitem
-add foreign key (l_l_id) references l_list(l_id) on delete restrict on update restrict;
-alter table t_token
-add foreign key (t_h_hastoken_u_id) references u_user(u_id) on delete restrict on update restrict;
+alter table sl_h_hasaccess
+add foreign key (h_p_id) references sl_p_priviledge(p_id) on delete restrict on update restrict,
+add foreign key (h_l_id) references sl_l_list(l_id) on delete restrict on update restrict,
+add foreign key (h_u_id) references sl_u_user(u_id) on delete restrict on update restrict;
+alter table sl_l_listitem
+add foreign key (l_l_id) references sl_l_list(l_id) on delete restrict on update restrict;
+alter table sl_t_token
+add foreign key (t_h_hastoken_u_id) references sl_u_user(u_id) on delete restrict on update restrict;
 -- commit all changes
-commit;
+-- commit;
