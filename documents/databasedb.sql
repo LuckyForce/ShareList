1aa5323d1e66901ce438de71673075a64a2797f8
LuckyForce <adrian.schauer@aon.at>
2022-02-28 10:53:19 +0100

    Added create list change name on list and invite user to list

--- documents/databasedb.sql
+++ documents/databasedb.sql
@@ -9,19 +9,20 @@
 -- set autocommit=0;
 -- tables-#################################################
 -- table h_hasaccess
-create table sl_h_hasaccess(
-     h_l_id varchar(100) not null,
-     h_u_id varchar(100) not null,
-     h_p_id varchar(100) not null,
-     h_priviledge varchar(100) not null,
-     primary key(h_l_id,h_u_id,h_p_id)
+create table sl_a_access(
+     a_l_id varchar(100) not null,
+     a_u_id varchar(100) not null,
+     a_p_id int AUTO_INCREMENT,
+     a_priviledge int not null,
+     primary key(a_l_id,a_u_id,a_p_id)
 );
 
 -- table l_list
 create table sl_l_list(
-     l_id varchar(100) not null,
-     l_createdate datetime not null,
-     l_title varchar(100) not null,
+     l_id varchar(255) not null,
+     l_created datetime not null,
+     l_name varchar(100) not null,
+     l_u_id int not null,
      primary key(l_id)
 );
 
@@ -43,10 +44,11 @@ create table sl_p_priviledge(
 
 -- table t_token
 create table sl_t_token(
-     t_id int primary key AUTO_INCREMENT,
+     t_id int AUTO_INCREMENT,
      t_u_id varchar(100) not null,
-     t_token varchar(100) not null,
-     t_expiration datetime not null
+     t_token varchar(255) not null,
+     t_expiration datetime not null,
+     primary key (t_id)
 );
 
 -- table u_user
@@ -55,21 +57,43 @@ create table sl_u_user(
      u_email varchar(100) not null,
      u_password varchar(255) not null,
      u_lastlogin datetime,
-     u_emailverified tinyint default 0,
-     u_emailcode varchar(100),
+     u_verified tinyint default 0,
+     u_verificationcode varchar(100),
      u_resetpwd varchar(100),
      u_resetpwdexpirationdate datetime
 );
 
+-- table i_invites
+create table sl_i_invites(
+     i_id varchar(255) not null,
+     i_l_id varchar(100) not null,
+     i_u_id varchar(100) not null,
+     i_invitedby varchar(100) not null,
+     i_created datetime not null,
+     i_accepted tinyint default 0,
+     i_accepteddate datetime,
+     i_p_id int not null,
+     primary key(i_id)
+);
 
 -- foreign keys-#################################################
-alter table sl_h_hasaccess
-add foreign key (h_p_id) references sl_p_priviledge(p_id) on delete restrict on update restrict,
-add foreign key (h_l_id) references sl_l_list(l_id) on delete restrict on update restrict,
-add foreign key (h_u_id) references sl_u_user(u_id) on delete restrict on update restrict;
+alter table sl_a_hasaccess
+add foreign key (a_p_id) references sl_p_priviledge(p_id) on delete restrict on update restrict,
+add foreign key (a_l_id) references sl_l_list(l_id) on delete restrict on update restrict,
+add foreign key (a_u_id) references sl_u_user(u_id) on delete restrict on update restrict;
 alter table sl_li_listitem
 add foreign key (li_l_id) references sl_l_list(l_id) on delete restrict on update restrict;
 alter table sl_t_token
 add foreign key (t_u_id) references sl_u_user(u_id) on delete restrict on update restrict;
+alter table sl_l_list
+add foreign key (l_u_id) references sl_u_user(u_id) on delete restrict on update restrict;
+alter table sl_i_invites
+add foreign key (i_l_id) references sl_l_list(l_id) on delete restrict on update restrict,
+add foreign key (i_u_id) references sl_u_user(u_id) on delete restrict on update restrict,
+add foreign key (i_invitedby) references sl_u_user(u_id) on delete restrict on update restrict;
+add foreign key (i_p_id) references sl_p_priviledge(p_id) on delete restrict on update restrict;
+-- #######################################################
 -- commit all changes
 -- commit;
+-- add priveledges write and read
+insert into sl_p_priviledge (p_id,p_description) values (1,'write'),(2,'read');
--- src/routes/api.php
+++ src/routes/api.php
@@ -18,11 +18,11 @@ use Illuminate\Support\Facades\Mail;
 |
 */
 
-//TODO: Change all DB::select or DB::insert or DB::update to DB::table
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
 
 //Register
 Route::post('/register', function (Request $request) {
@@ -33,19 +33,33 @@ Route::post('/register', function (Request $request) {
     }
 
     //Check if user already exists
-    $user = DB::table('sl_u_user')->where('u_email', $request->email)->where('u_emailverified', 1)->first();
+    $user = DB::table('sl_u_user')->where('u_email', $request->email)->where('u_verified', 1)->first();
     if ($user) {
         return response()->json(['message' => 'User already exists'], 400);
     }
 
+    //Check if the password is valid
+    if ($request->password == null || strlen($request->password) < 6 || !is_string($request->password)) {
+        return response()->json([
+            'error' => 'Password is invalid',
+        ], 400);
+    }
+
+    //Generate 6 Character long Code containing only numbers.
+    $numbers = "0123456789";
+    $code = "";
+    for ($i = 0; $i < 6; $i++) {
+        $code .= $numbers[rand(0, strlen($numbers) - 1)];
+    }
+
     //Check if user already exists but not already verified
-    $user = DB::table('sl_u_user')->where('u_email', $request->email)->where('u_emailverified', 0)->first();
+    $user = DB::table('sl_u_user')->where('u_email', $request->email)->where('u_verified', 0)->first();
     if ($user) {
         //User already exists. Overwrite Password.
-        DB::table('sl_u_user')->where('u_email', $request->email)->update(['u_password' => password_hash($request->password, PASSWORD_DEFAULT)]);
+        DB::table('sl_u_user')->where('u_email', $request->email)->update(['u_password' => password_hash($request->password, PASSWORD_DEFAULT), 'u_verificationcode' => $code]);
     }else{
         //Create new user
-        DB::table('sl_u_user')->insert(['u_email' => $request->email, 'u_password' => password_hash($request->password, PASSWORD_DEFAULT)]);
+        DB::table('sl_u_user')->insert(['u_email' => $request->email, 'u_password' => password_hash($request->password, PASSWORD_DEFAULT), 'u_verificationcode' => $code]);
     }
 
     //TODO: Send verification email
@@ -63,7 +77,7 @@ Route::post('/login', function (Request $request) {
     }
 
     //Get the user from the database
-    $user = DB::table('sl_u_user')->where('u_email', $request->email)->where('u_emailverified', 1)->first();
+    $user = DB::table('sl_u_user')->where('u_email', $request->email)->where('u_verified', 1)->first();
 
     //Check if the user exists
     if (!$user) { 
@@ -96,12 +110,12 @@ Route::post('/login', function (Request $request) {
 //Verify the user
 Route::post('/verify', function (Request $request) {
     //Validate data
-    if (!isset($request->code)){
-        return response()->json(['message' => 'Code is missing'], 400);
+    if (!isset($request->code) || !isset($request->email)){
+        return response()->json(['message' => 'Email or Code is missing'], 400);
     }
 
     //Get the user from the database
-    $user = DB::table('sl_u_user')->where('u_email', $request->email)->where('u_emailcode', $request->code)->first();
+    $user = DB::table('sl_u_user')->where('u_email', $request->email)->where('u_verificationcode', $request->code)->first();
 
     //Check if the user exists. By checking that you also check if the code is valid
     if (!$user) { 
@@ -111,7 +125,7 @@ Route::post('/verify', function (Request $request) {
     }
 
     //Update the user
-    DB::table('sl_u_user')->where('u_id', $user->u_id)->update(['u_emailcode' => null, 'u_emailverified' => 1]);
+    DB::table('sl_u_user')->where('u_id', $user->u_id)->update(['u_verificationcode' => null, 'u_verified' => 1]);
 
     //Return success
     return response()->json(['message' => 'User verified'], 200);
@@ -161,3 +175,134 @@ Route::post('/changepwd', function (Request $request) {
     //Return success
     return response()->json(['message' => 'Password changed'], 200);
 });
+
+//----------------------------------------------------------------------------------------------------------------------
+//List Routes
+//----------------------------------------------------------------------------------------------------------------------
+
+//Create List
+Route::post('createlist', function (Request $request) {
+    //Validate data
+    if (!isset($request->token) || !isset($request->name)){
+        return response()->json(['message' => 'Token or name is missing'], 400);
+    }
+
+    //Get the user from the database
+    $user = DB::table('sl_u_user')->join('sl_t_token', 't_u_id', '=', 'u_id')->where('t_token', $request->token)->where('t_expiration', '>', now()->toDateTimeString())->first();
+
+    //Check if the user exists. By checking that you also check if the token is valid
+    if (!$user) { 
+        return response()->json([
+            'error' => 'User not found',
+        ], 404);
+    }
+
+    //Create UUID
+    $uuid = Str::orderedUuid();
+    //Create List
+    DB::table('sl_l_list')->insert(['l_uuid' => $uuid, 'l_u_id' => $user->u_id, 'l_name' => 'Untitled', 'l_created' => now()->toDateTimeString()]);
+
+    //Return success
+    return response()->json(['message' => 'List created'], 200);
+});
+
+//Change List Name
+Route::post('changelistname', function (Request $request) {
+    //Validate data
+    if (!isset($request->token) || !isset($request->uuid) || !isset($request->name)){
+        return response()->json(['message' => 'Token, list or name is missing'], 400);
+    }
+
+    //Get the user from the database
+    $user = DB::table('sl_u_user')->join('sl_t_token', 't_u_id', '=', 'u_id')->where('t_token', $request->token)->where('t_expiration', '>', now()->toDateTimeString())->first();
+
+    //Check if the user exists. By checking that you also check if the token is valid
+    if (!$user) { 
+        return response()->json([
+            'error' => 'User not found',
+        ], 404);
+    }
+
+    //Get the list from the database
+    $list = DB::table('sl_l_list')->where('l_uuid', $request->uuid)->where('l_u_id', $user->u_id)->first();
+
+    //Check if the list exists. By checking that you also check if the user is the owner of the list
+    if (!$list) { 
+        return response()->json([
+            'error' => 'List not found',
+        ], 404);
+    }
+
+    //Validate the name
+    if ($request->name == null || strlen($request->name) < 1 || !is_string($request->name)) {
+        return response()->json([
+            'error' => 'Name is invalid',
+        ], 400);
+    }
+
+    //Update the list
+    DB::table('sl_l_list')->where('l_uuid', $request->uuid)->where('l_u_id', $user->u_id)->update(['l_name' => $request->name]);
+
+    //Return success
+    return response()->json(['message' => 'List name changed'], 200);
+});
+
+//Add User to List
+Route::post('invitetolist', function (Request $request) {
+    //Validate data
+    if (!isset($request->token) || !isset($request->uuid) || !isset($request->email)){
+        return response()->json(['message' => 'Token, list or user is missing'], 400);
+    }
+
+    //Get the user from the database
+    $user = DB::table('sl_u_user')->join('sl_t_token', 't_u_id', '=', 'u_id')->where('t_token', $request->token)->where('t_expiration', '>', now()->toDateTimeString())->first();
+
+    //Check if the user exists. By checking that you also check if the token is valid
+    if (!$user) { 
+        return response()->json([
+            'error' => 'User not found',
+        ], 404);
+    }
+
+    //Check invited user from the database
+    $invitedUser = DB::table('sl_u_user')->where('u_email', $request->email)->where('u_verified', 1)->first();
+
+    //Check if the invited user exists. By checkking that you also check if the user is verified
+    if (!$invitedUser) { 
+        return response()->json([
+            'error' => 'Invited user not found',
+        ], 404);
+    }
+
+    //Get the list from the database
+    $list = DB::table('sl_l_list')->where('l_uuid', $request->uuid)->where('l_u_id', $user->u_id)->first();
+
+    //Check if the list exists. By checking that you also check if the user is the owner of the list
+    if (!$list) {
+        return response()->json([
+            'error' => 'List not found',
+        ], 404);
+    }
+
+    //Check if the user is already in the list
+    $userInList = DB::table('sl_l_list')->join('sl_a_access', 'a_l_id', '=', 'l_id')->where('l_id', $request->uuid)->where('a_u_id', $invitedUser->u_id)->first();
+
+    if ($userInList) {
+        return response()->json([
+            'error' => 'User is already in the list',
+        ], 400);
+    }
+
+    //Create UUID
+    $uuid = Str::orderedUuid();
+
+    //Create Invite to add user to list
+    DB::table('sl_i_invite')->insert(['i_id' => $uuid, 'i_l_id' => $list->l_id, 'i_u_id' => $invitedUser->u_id, 'i_created' => now()->toDateTimeString(), 'i_p_id' => 2]);
+
+    //TODO: Send Invite Email
+    
+
+    //Return success
+    return response()->json(['message' => 'User invited to list'], 200);
+});
+
