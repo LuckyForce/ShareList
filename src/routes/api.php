864d8168a0d671cc877fa74dd7d7767d56e122ee
LuckyForce <adrian.schauer@aon.at>
2022-03-11 23:02:36 +0100

    Added write attribute

--- src/routes/api.php
+++ src/routes/api.php
@@ -340,9 +340,13 @@ Route::post('/list', function (Request $request) {
     //Check if the user is owner of the list.
     $admin = DB::table('sl_l_list')->where('l_id', $list->l_id)->where('l_u_id', $user->u_id)->first();
     if($admin) $admin = true; else $admin = false;
+
+    //Check if the user has write permission for the list.
+    $write = DB::table('sl_a_access')->where('a_l_id', $list->l_id)->where('a_u_id', $user->u_id)->where('a_p_id', 2)->first();
+    if($write) $write = true; else $write = false;
     
     //Return list
-    return response()->json(['list' => $list, 'items' => $items, 'admin' => $admin], 200);
+    return response()->json(['list' => $list, 'items' => $items, 'admin' => $admin, 'write' => $write], 200);
 });
 
 
