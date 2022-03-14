a26f5996180b68f8e7a7ffc2df56b3911d568b31
LuckyForce <adrian.schauer@aon.at>
2022-03-14 10:55:56 +0100

    Removed unecessary file and added owner page component

---
+++ src/resources/views/owner.blade.php
@@ -0,0 +1,6 @@
+<?php 
+    //This Page will be shown if the user enters a page where only the list owner should be.
+?>
+<div id="not-owner">
+    <h1>You are not the owner of this list.</h1>
+</div>
