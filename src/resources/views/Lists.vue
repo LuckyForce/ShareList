a1fe88c0914084fea00b6fbefb4114ba55c1c35e
Bobstarius <robiermler3@gmail.com>
2022-04-17 16:33:09 +0200

    Frontend: Home and Lists bugs fixed

--- src/resources/views/Lists.vue
+++ src/resources/views/Lists.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1 class="mt-6 w-screen text-4xl flex justify-center ">Your current Lists</h1>
+        <h1 class="mt-6 text-4xl flex justify-center ">Your current Lists</h1>
         <div class="w-full my-8 gap-24 flex-wrap flex justify-center items-center">
             <div class="w-60 p-2 bg-white rounded-xl p-2">
                 <h2 class="text-xl mb-2 ml-1"> Listname </h2>
@@ -53,6 +53,7 @@
                 <button type="submit" class="btn-show1">Show</button>
             </div>
         </div>
+        <button type="submit" class="btn-create text-2xl mb-6 ml-10">Create a List</button>
     </div>
 </template>
 
