3aaa5e790047f23f7c8c9fede1b29b426fdd547e
Bobstarius <robiermler3@gmail.com>
2022-04-17 15:59:51 +0200

    Frontend: CSS, Home and Lists changed

--- src/resources/views/Home.vue
+++ src/resources/views/Home.vue
@@ -4,20 +4,26 @@
         Die Bilder müssten noch richtig implementiert werden und der richtige Text
         <h1 class="mt-6 w-screen text-4xl flex justify-center ">Features</h1>
         <div class="w-full my-8 gap-24 flex-wrap flex justify-center items-center"> 
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
             </div>
             </div>
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
             </div>
             </div>
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="p-2">
+                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+            </div>
+            </div>
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
             </div>
@@ -25,20 +31,26 @@
         </div>
         <h1 class="mt-6 w-screen text-4xl flex justify-center ">Benefits</h1>
         <div class="w-full my-8 gap-24 flex-wrap flex justify-center items-center"> 
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
             </div>
             </div>
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
             </div>
             </div>
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="p-2">
+                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+            </div>
+            </div>
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
             </div>
@@ -46,20 +58,26 @@
         </div>
         <h1 class="mt-6 w-screen text-4xl flex justify-center ">How to start</h1>
         <div class="w-full my-8 gap-24 flex-wrap flex justify-center items-center"> 
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
             </div>
             </div>
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
             </div>
             </div>
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
+            <div class="p-2">
+                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+            </div>
+            </div>
+            <div class="w-60 p-2 bg-white rounded-xl">
+                <img class="h-40 rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
             </div>
--- src/resources/views/Lists.vue
+++ src/resources/views/Lists.vue
@@ -1,24 +1,56 @@
 <template>
     <div>
         <h1 class="mt-6 w-screen text-4xl flex justify-center ">Your current Lists</h1>
-        <div class="w-full my-8 gap-24 flex-wrap flex justify-center items-center"> 
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
-            <div class="p-2">
-                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+        <div class="w-full my-8 gap-24 flex-wrap flex justify-center items-center">
+            <div class="w-60 p-2 bg-white rounded-xl p-2">
+                <h2 class="text-xl mb-2 ml-1"> Listname </h2>
+                <p class="text-sm text-gray-600 mb-2 ml-1">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                <button type="submit" class="btn-delete1">Delete</button>
+                <button type="submit" class="btn-show1">Show</button>
             </div>
+            <div class="w-60 p-2 bg-white rounded-xl p-2">
+                <h2 class="text-xl mb-2 ml-1"> Listname </h2>
+                <p class="text-sm text-gray-600 mb-2 ml-1">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                <button type="submit" class="btn-delete1">Delete</button>
+                <button type="submit" class="btn-show1">Show</button>
             </div>
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
-            <div class="p-2">
-                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+            <div class="w-60 p-2 bg-white rounded-xl p-2">
+                <h2 class="text-xl mb-2 ml-1"> Listname </h2>
+                <p class="text-sm text-gray-600 mb-2 ml-1">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                <button type="submit" class="btn-delete1">Delete</button>
+                <button type="submit" class="btn-show1">Show</button>
             </div>
+            <div class="w-60 p-2 bg-white rounded-xl p-2">
+                <h2 class="text-xl mb-2 ml-1"> Listname </h2>
+                <p class="text-sm text-gray-600 mb-2 ml-1">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                <button type="submit" class="btn-delete1">Delete</button>
+                <button type="submit" class="btn-show1">Show</button>
             </div>
-            <div class="w-60 p-2 bg-white rounded-xl transform transition-all">
-                <img class="h-40 object-cover rounded-xl" h-40="" object-cover="" rounded-xl="" src="https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg" alt="">
-            <div class="p-2">
-                <p class="text-sm text-gray-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+        </div>
+        <div class="w-full my-8 gap-24 flex-wrap flex justify-center items-center">
+            <div class="w-60 p-2 bg-white rounded-xl p-2">
+                <h2 class="text-xl mb-2 ml-1"> Listname </h2>
+                <p class="text-sm text-gray-600 mb-2 ml-1">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                <button type="submit" class="btn-delete1">Delete</button>
+                <button type="submit" class="btn-show1">Show</button>
             </div>
+            <div class="w-60 p-2 bg-white rounded-xl p-2">
+                <h2 class="text-xl mb-2 ml-1"> Listname </h2>
+                <p class="text-sm text-gray-600 mb-2 ml-1">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                <button type="submit" class="btn-delete1">Delete</button>
+                <button type="submit" class="btn-show1">Show</button>
+            </div>
+            <div class="w-60 p-2 bg-white rounded-xl p-2">
+                <h2 class="text-xl mb-2 ml-1"> Listname </h2>
+                <p class="text-sm text-gray-600 mb-2 ml-1">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                <button type="submit" class="btn-delete1">Delete</button>
+                <button type="submit" class="btn-show1">Show</button>
+            </div>
+            <div class="w-60 p-2 bg-white rounded-xl p-2">
+                <h2 class="text-xl mb-2 ml-1"> Listname </h2>
+                <p class="text-sm text-gray-600 mb-2 ml-1">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                <button type="submit" class="btn-delete1">Delete</button>
+                <button type="submit" class="btn-show1">Show</button>
             </div>
         </div>
     </div>
