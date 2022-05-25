e9a1b400ce9e2ba8c1b8c333abc067547ff9a202
Flayser <las20298@spengergasse.at>
2022-05-25 14:11:07 +0200

    ListMembers Erstellt

--- src/resources/views/ListMembers.vue
+++ src/resources/views/ListMembers.vue
@@ -14,7 +14,7 @@
             </router-link>
         </div>
         <h1 class="mt-6 text-4xl flex justify-center">List Of Users</h1>
-    
+
         <div class="flex flex-col w-full justify-center my-8">
             <div class="md:w-2/6 w-4/6 mx-auto flex flex-col">
                 <div
@@ -23,13 +23,36 @@
                     class="list-card mb-1 flex mt-1"
                     @click="this.getList(list.in_id)"
                 >
-                    <p class="ml-1 md:w-3/6 mr-auto text-lg mt-3">
+                    <p class="ml-1 md:w-3/6 mr-auto text-lg mt-1">
                         {{ invite.in_email }}
                     </p>
-                    <div class="flex">
-                    <input type="checkbox" id="choose" value="HTML">
+                    <div
+                        v-if="invite.write"
+                        class="m-auto">
+                        <svg
+                            class="w-4 h-4 fill-current text-gray-500"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"
+                            />
+                        </svg>
+                    </div>
+                    <div class="flex mt-auto mb-auto">
+                        <input type="checkbox" id="choose" value="HTML" />
                     </div>
                 </div>
+            </div>
+            <div class="flex justify-center space-x-4">
+                <button class="btn-create flex justify-center">
+                    Add Write
+                </button>
+                <button class="btn-create flex justify-center">
+                    Remove Write
+                </button>
+                <button class="btn-delete1 flex justify-center">
+                    Delete User
+                </button>
             </div>
         </div>
     </div>
@@ -44,16 +67,19 @@ export default {
                     in_id: 1,
                     in_email: "hgjdsk@gmail.com",
                     in_created: "2020-05-05",
+                    write: true,
                 },
                 {
                     in_id: 2,
                     in_email: "kkej@gmail.com",
                     in_created: "2020-05-05",
+                    write: false,
                 },
                 {
                     in_id: 3,
                     in_email: "iohn@gmailc.com",
                     in_created: "2020-05-05",
+                    write: true,
                 },
             ],
         };
