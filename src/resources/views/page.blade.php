ea330241cc4171a7c0cb3e2454f803fff2af5383
LuckyForce <adrian.schauer@aon.at>
2022-05-27 16:51:55 +0200

    Meta Data

--- src/resources/views/page.blade.php
+++ src/resources/views/page.blade.php
@@ -7,6 +7,20 @@
     <title>ShareList</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="icon" href="{{ url('images/logo.png') }}">
+
+    <!-- Meta Daten -->
+    <meta name="twitter:card" content="summary_small_image">
+    <meta id="image-src" name="twitter:image:src" content="{{ url('images/logo.png') }}">
+    <meta id="discord" name="twitter:image" content="{{ url('images/logo.png') }}">
+    <meta id="embed-title" property="og:title" content="ShareList">
+    <meta id="embed-desc" property="og:description" content="ShareList, a list easy to share.">
+    <meta id="embed-image" property="og:image" content="{{ url('images/logo.png') }}">
+    <meta name="theme-color" content="#0fa8d6">
+
+    <!-- Suchmaschinen Meta Daten -->
+    <meta name="robots" content="index"/>
+    <meta name="description" content="ShareList, a list easy to share." />
+    <meta name="keywords" content="Adrian, Schauer, Eichgraben, Österreich, Wien, Niederösterreich, Spengergasse, List, ToDo, ShareList">
 </head>
 <body>
     <div id="app" class="h-screen flex flex-col">
