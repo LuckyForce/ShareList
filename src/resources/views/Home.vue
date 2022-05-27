4af8a12afd5f2bd82f77a063bd6fdf94794bf54c
Bobstarius <robiermler3@gmail.com>
2022-05-27 12:58:07 +0200

    Images: updated

--- src/resources/views/Home.vue
+++ src/resources/views/Home.vue
@@ -4,31 +4,31 @@
         <h1 class="mt-6 text-4xl flex justify-center ">Features</h1>
         <div class="w-full my-8 gap-10 flex-wrap flex justify-center items-center"> 
             <div class="w-60 p-2 bg-white rounded-xl">
-                <img class="h-40 rounded-xl" src="/images/UnlimitedLists.jpg" alt="">
+                <img class="h-40 rounded-xl" src="/images/unlimitedlists.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">You can create as many lists as you need for every part of your life with a simple click on a button.</p>
             </div>
             </div>
             <div class="w-60 p-2 bg-white rounded-xl">
-                <img class="h-40 rounded-xl w-60" src="/images/UnlimitedListElements.jpg" alt="">
+                <img class="h-40 rounded-xl w-60" src="/images/unlimitedlistelements.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">You can create as many entries into the list as you want mark those green which you have already done.</p>
             </div>
             </div>
             <div class="w-60 p-2 bg-white rounded-xl">
-                <img class="h-40 rounded-xl w-60" src="/images/ListEdit.jpg" alt="">
+                <img class="h-40 rounded-xl w-60" src="/images/listedit.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">You can also edit the name and description of the List but also give the ownership to another user or delete it.</p>
             </div>
             </div>
             <div class="w-60 p-2 bg-white rounded-xl">
-                <img class="h-40 rounded-xl" src="/images/ListOfUsers.jpg" alt="">
+                <img class="h-40 rounded-xl" src="/images/listofusers.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">You can change the permission of the users aswell like giving another user permission to add entries or you can delete a user.</p>
             </div>
             </div>
             <div class="w-60 p-2 bg-white rounded-xl">
-                <img class="h-40 rounded-xl w-60" src="/images/AddUsers.jpg" alt="">
+                <img class="h-40 rounded-xl w-60" src="/images/addusers.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">You can invite as many users as you want to your list and if you mistakenly invited someone you can delete the invite permission.</p>
             </div>
@@ -37,19 +37,19 @@
         <h1 class="mt-6 text-4xl flex justify-center ">Benefits</h1>
         <div class="w-full my-8 gap-24 flex-wrap flex justify-center items-center"> 
             <div class="w-60 p-2 bg-white rounded-xl">
-                <img class="h-40 rounded-xl" src="/images/List.jpg" alt="">
+                <img class="h-40 rounded-xl" src="/images/list.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">One benefit of using our webpage and not others for creating lists would be the ease of creating lists on our webpage.</p>
             </div>
             </div>
             <div class="w-60 p-2 bg-white rounded-xl">
-                <img class="h-40 rounded-xl w-60" src="/images/UnlimitedListElements.jpg" alt="">
+                <img class="h-40 rounded-xl w-60" src="/images/unlimitedlistelements.jpg" alt="">
             <div class="p-2">s
                 <p class="text-sm text-gray-600">The many options to customize and manage your lists are also a great benefit, combined with the ease of using all the tools.</p>
             </div>
             </div>
             <div class="w-60 p-2 bg-white rounded-xl">
-                <img class="h-40 rounded-xl" src="/images/ListOfUsers.jpg" alt="">
+                <img class="h-40 rounded-xl" src="/images/listofusers.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">The ability to share your list with multiple people and be able to make edits from anywhere in the world is also incredible.</p>
             </div>
@@ -70,13 +70,13 @@
             </div>
             </div>
             <div class="w-60 p-2 bg-white rounded-xl">
-                <img class="h-40 rounded-xl" src="/images/List.jpg" alt="">
+                <img class="h-40 rounded-xl" src="/images/list.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">First you have to press on Lists in the top left corner. Afterwards you can press the button Create List through which the fun part will now start!</p>
             </div>
             </div>
             <div class="w-60 p-2 bg-white rounded-xl">
-                <img class="h-40 rounded-xl" src="/images/ListCreated.jpg" alt="">
+                <img class="h-40 rounded-xl" src="/images/listcreated.jpg" alt="">
             <div class="p-2">
                 <p class="text-sm text-gray-600">Once you created your own List you can now add things to do but also change the name of the List or add people who are allowed to view it.</p>
             </div>
