47e89061a42ec5daf2431e8f2602c0c1e9059af7
Flayser <las20298@spengergasse.at>
2022-05-28 19:57:07 +0200

    Header: Hover funktion hinzugefügt

--- src/resources/components/Header.vue
+++ src/resources/components/Header.vue
@@ -2,27 +2,27 @@
 <template>
     <nav class="flex items-center justify-between flex-wrap color-2">
         <div class="flex justify-center align-center">
-            <router-link to="/" class="flex items-center nav-link ml-1">
+            <router-link to="/" class="flex items-center nav-link ml-1 header">
                 <img :src="imgPath" alt="Logo" class="sm:h-12 h-8" />
                 Home
             </router-link>
             <router-link
                 v-if="isLoggedIn"
                 to="/lists"
-                class="flex items-center nav-link"
+                class="flex items-center nav-link header"
             >
                 Lists
             </router-link>
         </div>
         <div class="flex items-center flex-shrink-0 text-white">
-            <router-link to="/about" class="nav-link">About Us</router-link>
+            <router-link to="/about" class="nav-link header">About Us</router-link>
             <router-link
                 v-if="isLoggedIn === false"
                 to="/register"
-                class="nav-link"
+                class="nav-link header"
                 >Register</router-link
             >
-            <router-link v-if="isLoggedIn" to="/profile" class="nav-link"
+            <router-link v-if="isLoggedIn" to="/profile" class="nav-link header"
                 >Profile</router-link
             >
         </div>
